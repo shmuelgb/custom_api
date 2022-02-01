88f0206c977ddaba4608b72f24e2658371f00fac
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-01 12:49:05 +0200

    psudo code for backend

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
+const cors = require("cors");
+const usersRoute = require("./router/usersRoute");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", usersRoute);
 
 const publicDirectoryPath = path.join(__dirname, "client/build");
 app.use(express.static(publicDirectoryPath));
