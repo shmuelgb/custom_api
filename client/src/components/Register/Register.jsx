c539d7dce3ff6f1146c57e566eefd06b46afed8f
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-07 15:29:07 +0200

    Register compo

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,6 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { useTokenPro } from "../../providers/SessionProvider";
+import caServer from "../../api/ca_server";
+import Background from "../Background/Background";
 import "./Register.css";
 
 export default function Register() {
-  return <div className="Register"></div>;
+  const [token, setToken] = useTokenPro();
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const { data } = await caServer.post("/signIn", {
+        name,
+        email,
+        password,
+      });
+      setToken(data);
+      localStorage.setItem("token", data.token);
+      history.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (token) history.push("/dashboard");
+  }, [token, history]);
+
+  return (
+    <div className="Register">
+      <Background />
+      <div className="register__container">
+        <h1>Sign In</h1>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full Name"
+        />
+        <input
+          type="text"
+          name="email"
+          className="register__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="register__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className="btn" onClick={handleRegister}>
+          Register
+        </button>
+        <div>
+          Already have an account?
+          <br />
+          <Link className="login" to="/login">
+            Login now
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
