a47678edcade915e6b16a6bd9d3040727dbc3b28
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-08 12:26:35 +0200

    basic Dashboard page layout

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect } from "react";
-import { useTokenPro } from "../../providers/SessionProvider";
+import { useTokenPro, useUserInfoPro } from "../../providers/SessionProvider";
 import { useHistory } from "react-router-dom";
+import "./styles/Dashboard.css";
 
 export default function Dashboard() {
   const [token] = useTokenPro();
+  const [userInfo] = useUserInfoPro();
   const history = useHistory();
 
   useEffect(() => {
     if (!token) history.push("/login");
   }, [token, history]);
 
+  const generateCards = () => {};
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-      nostrum eos vitae eum temporibus quisquam laboriosam, ea veniam sequi
-      assumenda expedita ipsa doloremque praesentium officiis maiores commodi ut
-      est hic?
+    <div className="Dashboard">
+      <h1>Hello {userInfo.name}!</h1>
+      <h2>Here's Your API:</h2>
+      <div className="dashboard-content">
+        <div className="dashboard-content__cards">{generateCards()}</div>
+        <div className="dashboard-content__controllers">
+          <button>Create new resource</button>
+          <button>How To Use?</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Dashboard/ResourceCard.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles/ResourceCard.css";
+
+export default function ResourceCard() {
+  return <div className="ResourceCard">ResourceCard</div>;
+}
