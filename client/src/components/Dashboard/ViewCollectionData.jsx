56f7e22652d88a895f7b691bd98e699ea0c1ab6a
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-02-09 10:02:38 +0200

    popup animation

--- client/src/components/Dashboard/ViewCollectionData.jsx
+++ client/src/components/Dashboard/ViewCollectionData.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import "./styles/ViewCollectionData.css";
 
 export default function ViewCollectionData({ collectionData, setPopup }) {
+  const PrettyPrintJson = React.memo(({ data }) => (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  ));
+
   const printData = () => {
     if (collectionData) {
-      let str = JSON.stringify(collectionData, null, " ");
-      str = str.replace(/\s\s+/g, " ");
-      return str.split(" ").map((line, i) => {
-        return (
-          <p key={i}>
-            {line} <br />
-          </p>
-        );
-      });
+      return <PrettyPrintJson data={collectionData} />;
     }
   };
 
